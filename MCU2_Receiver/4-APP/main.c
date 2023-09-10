f33a20974679fe210c43133892555a685821e8e0
EhabMagdyy <ehabmagdy0044@gmail.com>
2023-09-11 01:45:06 +0300

    Updating Receiver

--- MCU2_Receiver/4-APP/main.c
+++ MCU2_Receiver/4-APP/main.c
@@ -10,93 +10,47 @@
 #include <string.h>
 
 void application_init(void);
-void follow_abnormal_5Sec(void);
+void Abnormal_state_5Sec(void);
+void Emeregency_state(void);
+void Normal_state(void);
+void checkStringAvalabile(void);
+void Timer2_InterruptHandler(void);
+void Set_Machine_speed(void);
 
 u16 digitalValue = 0;
+u8 received_msg[4];
 
 u8 volatile Flag = 0;
-void checkStringAvalabile(void){Flag = 1;}
-
 volatile u8 Timer2_Falg;
-void Timer2_InterruptHandler(void)
-{
-	Timer2_Falg = 1;
-}
 
 int main()
 {
 	application_init();
-	GI_enu_Enable();
-	DC_Motor_enu_Rotate(MOTOR1,CW);
-
-	u8 str[4];
 
 	while(1)
 	{
-		u8 speed = 0;
-		ADC_enu_GetDigitalValueSynchNonBlocking(ADC_U8_CHANNEL0,&digitalValue);
-		speed =(digitalValue * 255UL)/1023;
-		Timers_enu_Timer0SetCompareValue(speed);
+		UART_enu_receiveStringWithInterrrupt(received_msg,&checkStringAvalabile,3);
 
-		UART_enu_receiveStringWithInterrrupt(str,&checkStringAvalabile,3);
-
-		if(Flag == 1){
-			LCD_enu_Init();
-			LCD_enu_SendString(str);
-			if(!strcmp(str,"EM\r"))
+		if(Flag == 1)
+		{
+			if(!strcmp(received_msg,"EM\r"))
 			{
-				DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_HIGH);
-				DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_HIGH);
-				DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN1,DIO_U8_LOW);
-				DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN2,DIO_U8_LOW);
+				Emeregency_state();
 			}
-			else if(!strcmp(str,"SM\r"))
+			else if(!strcmp(received_msg,"SM\r"))
 			{
 				/*  stop the machine */
+				LCD_enu_Clear();
+				LCD_enu_SendString("Stop Machine");
 				DC_Motor_enu_Stop(MOTOR1);
 			}
-			else if(!strcmp(str,"AB\r"))
+			else if(!strcmp(received_msg,"AB\r"))
 			{
-				follow_abnormal_5Sec();
+				Abnormal_state_5Sec();
 			}
 			else
 			{
-				u8 i = 0;
-				u8 temp = 0;
-				while(str[i] != '\r'){
-					temp = (temp*10) + (str[i] - '0');
-					i++;
-				}
-				LCD_enu_Init();
-				LCD_enu_SendNum(temp);
-				if(temp < 20)
-				{
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN2,DIO_U8_HIGH);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_LOW);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN1,DIO_U8_LOW);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_LOW);
-				}
-				else if((temp >= 20)&&(temp <= 40))
-				{
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN1,DIO_U8_HIGH);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_LOW);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN2,DIO_U8_LOW);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_LOW);
-				}
-				else if ((temp > 40)&&(temp <= 50))
-				{
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_HIGH);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN1,DIO_U8_LOW);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN2,DIO_U8_LOW);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_LOW);
-				}
-				else
-				{
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_HIGH);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_HIGH);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN1,DIO_U8_LOW);
-					DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN2,DIO_U8_LOW);
-				}
+				Normal_state();
 			}
 			Flag = 0;
 		}
@@ -122,10 +76,32 @@ void application_init(void)
 	DC_Motor_enu_Init(1,0);
 
 	LCD_enu_Init();
+	GI_enu_Enable();
+	DC_Motor_enu_Rotate(MOTOR1,CW);
 }
 
-void follow_abnormal_5Sec(void)
+void checkStringAvalabile(void)
 {
+	Flag = 1;
+}
+
+void Timer2_InterruptHandler(void)
+{
+	Timer2_Falg = 1;
+}
+
+void Set_Machine_speed(void)
+{
+	u8 speed = 0;
+	ADC_enu_GetDigitalValueSynchNonBlocking(ADC_U8_CHANNEL0,&digitalValue);
+	speed =(digitalValue * 255UL)/1023;
+	Timers_enu_Timer0SetCompareValue(speed);
+}
+
+void Abnormal_state_5Sec(void)
+{
+	LCD_enu_Clear();
+	LCD_enu_SendString("Abnormal State");
     /* Count 5 Seconds */
 	Timer2_voidStartCounting();
 	/* lighten red LED */
@@ -140,6 +116,64 @@ void follow_abnormal_5Sec(void)
 	/* Wait 5 Seconds */
 	while(0 == Timer2_Falg);
 	Timer2_Falg = 0;
-
 	Timer2_voidDisableCounting();
+
+	DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_LOW);
+	DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_LOW);
+	Servo_enu_SetAngle(MOTOR_1, 0);
+}
+
+void Emeregency_state(void)
+{
+	LCD_enu_Clear();
+	LCD_enu_SendString("Emergency State");
+	/* lighten red LED & run buzzer */
+	DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_HIGH);
+	DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_HIGH);
+	DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN1,DIO_U8_LOW);
+	DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN2,DIO_U8_LOW);
+}
+
+void Normal_state(void)
+{
+	u8 i = 0;
+	u8 temp = 0;
+	while(received_msg[i] != '\r'){
+		temp = (temp*10) + (received_msg[i] - '0');
+		i++;
+	}
+	LCD_enu_Clear();
+	LCD_enu_SendString("Normal State");
+	LCD_enu_SetCursor(1, 0);
+	LCD_enu_SendString("Temp: ");
+	LCD_enu_SetCursor(1, 6);
+	LCD_enu_SendNum(temp);
+
+	if(temp < 20)
+	{
+		/* turn on green led */
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN2,DIO_U8_HIGH);
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_LOW);
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN1,DIO_U8_LOW);
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_LOW);
+	}
+	else if((temp >= 20)&&(temp <= 40))
+	{
+		/* turn on yellow led */
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN1,DIO_U8_HIGH);
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_LOW);
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN2,DIO_U8_LOW);
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_LOW);
+	}
+	else if ((temp > 40)&&(temp <= 50))
+	{
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN0,DIO_U8_HIGH);
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN1,DIO_U8_LOW);
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN2,DIO_U8_LOW);
+		DIO_enu_SetPinValue(DIO_U8_PORTC,DIO_U8_PIN3,DIO_U8_LOW);
+	}
+	else
+	{
+		Emeregency_state();
+	}
 }
